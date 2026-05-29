9adf18e3e527e07e19bbe99225a9f12a903e1835
Владимир Фролов <frolov@dna-tech.lo>
2026-05-29 14:43:09 +0300

    recursion added to specapp in the main branch

---
+++ SpecApp.py
@@ -0,0 +1,26 @@
+# Test GIT
+for i in range(2, 7):
+    if i % 2 == 0:
+        print(f'Even number{i}')
+        continue
+    print(f'Odd number {i}')
+
+print('Lambda: ', list(map(lambda x: x + 3, [1, 2, 3])))  #final version
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+for row in matrix:
+    for element in row:
+        print(f' Matrix element {element}')
+
+
+# Test remote branch
+# Recursion on main special-app branch
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+print(f'Factorial = ', factorial(5))
