48ab417b7138419e32bf5dc4ec514e92cc10def4
Владимир Фролов <frolov@dna-tech.lo>
2026-06-02 11:13:27 +0300

    commit test_git (lambda adde), spec-app, master

--- test_git.py
+++ test_git.py
@@ -1,6 +1,13 @@
 # test file for git
 n = 10
+print(n)
 for i in range(n):
     n -= 1
-    #print(len(n))
+
     print(n)
+
+
+lmb = lambda a, b, c: a + b + c
+
+# a,b,c = 1 ,2, 3
+print(lmb(1, 2, 3))
