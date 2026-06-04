a5c94a9cb46976ed1a0966d0f8d1b762bfbed0f8
Владимир Фролов <frolov@dna-tech.lo>
2026-06-04 15:19:30 +0300

    changes in two files for testing

--- SpecApp.py
+++ SpecApp.py
@@ -7,7 +7,7 @@ for i in range(2, 7):
 
 print('Lambda: ', list(map(lambda x: x + 3, [1, 2, 3])))  #final version
 
-matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
 
 for row in matrix:
     for element in row:
--- test_git.py
+++ test_git.py
@@ -4,3 +4,5 @@ for i in range(n):
     n -= 1
     #print(len(n))
     print(n)
+
+matrix = [[1, 2], [3, 4], [5, 6]]
